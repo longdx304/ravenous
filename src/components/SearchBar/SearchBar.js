b8a8f9d075d3f064ac2d8479192fbbcd99fa891c
Long Do <longdx@vfdcorp.com>
2019-09-03 13:05:15 +0700

    restyle code

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -52,13 +52,11 @@ class SearchBar extends React.Component {
     renderSortByOptions() {
         return Object.keys(this.sortByOptions).map(sortByOption => {
             let sortByOptionValue = this.sortByOptions[sortByOption];
-            return <li 
-                        key={sortByOptionValue} 
+            return (<li key={sortByOptionValue} 
                         className={this.getSortByClass(sortByOptionValue)} 
-                        onClick={this.handleSortByChange.bind(this, sortByOptionValue)}
-                    >
+                        onClick={this.handleSortByChange.bind(this, sortByOptionValue)}>
                         {sortByOption}
-                    </li>;
+                    </li>);
         });
     }
 
